d31f3126610203a41b32a83aee859174e0f25b73
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2020-06-22 12:02:42 -0500

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+
+    update = () => {
+        console.log('Entering password...')
+    }
+
+    render() {
+        return(
+            <div>
+                <input onKeyUp={this.update} type="password"></input>
+            </div>
+        )
+    }
+}
